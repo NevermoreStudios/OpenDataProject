8acede3215b198beaa5d2c09d72ef6d4d6a2a60b
Lazar Premovic <lazar2premovic@gmail.com>
2016-04-22 07:41:55 +0200

    loader

---
+++ OpenDataProject/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenDataProject
+{
+    public partial class OpenDataProject : Form
+    {
+        public OpenDataProject()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDataProject_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.Columns.Add("ID", "ID");
+            dataGridView1.Columns.Add("Ime", "Ime");
+            dataGridView1.Columns.Add("Adresa", "Adresa");
+            dataGridView1.Columns.Add("Postanski Broj", "Postanski Broj");
+            dataGridView1.Columns.Add("Mesto", "Mesto");
+            dataGridView1.Columns.Add("Opstina", "Opstina");
+            dataGridView1.Columns.Add("Okrug", "Okrug");
+            dataGridView1.Columns.Add("Skolska Uprava", "Skolska Uprava");
+            dataGridView1.Columns.Add("Sajt", "Sajt");
+            dataGridView1.Columns.Add("Telefon", "Telefon");
+            dataGridView1.Columns.Add("Fax", "Fax");
+            dataGridView1.Columns.Add("Tip Skole", "Tip Skole");
+            dataGridView1.Columns.Add("Broj Odeljenja", "Broj Odeljenja");
+            dataGridView1.Columns.Add("Latitude", "Latitude");
+            dataGridView1.Columns.Add("Longitude", "Longitude");
+            WebClient client = new WebClient();
+            client.DownloadFile("http://opendata.mpn.gov.rs/get.php?dataset=skole&lang=sr&term=csv", "data.csv");
+            StreamReader sr = new StreamReader("data.csv");
+            sr.ReadLine();
+            sr.ReadLine();
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                OpenDataProjectCore.Skoles.Add(sr.ReadLine());
+                
+            }
+        }
+    }
+}
---
+++ OpenDataProject/OpenDataProjectCore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDataProject
+{
+    static class OpenDataProjectCore
+    {
+        public static List<skola> Skole=new List<skola>();
+        public static List<string> Skoles =new List<string>();
+    }
+
+    class skola
+    {
+        int ID;
+        string Ime;
+        string Adresa;
+        string PostanskiBroj;
+        string Mesto;
+        string Opstina;
+        string Okrug;
+        string SkolskaUprava;
+        string Sajt;
+        string Telefon;
+        string Fax;
+        string TipSkole;
+        int BrojOdeljenja;
+        int Lat;
+        int Lon;
+        public skola(int ID,string Ime,string Adresa,string PostanskiBroj,string Mesto,string Opstina,string Okrug,string SkolskaUprava,string Sajt,string Telefon,string Fax,string TipSkole,int BrojOdeljenja,int Lat,int Lon)
+        {
+            ID=ID;
+            Ime=Ime;
+            Adresa=Adresa;
+            PostanskiBroj=PostanskiBroj;
+            Mesto=Mesto;
+            Opstina=Opstina;
+            Okrug=Okrug;
+            SkolskaUprava=SkolskaUprava;
+            Sajt=Sajt;
+            Telefon=Telefon;
+            Fax=Fax;
+            TipSkole=TipSkole;
+            BrojOdeljenja=BrojOdeljenja;
+            Lat=Lat;
+            Lon=Lon;
+        }
+    }
+}
