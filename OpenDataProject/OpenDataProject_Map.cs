04a47c79d6b25d3a2655a3fbda2cf83ba6600240
Lazar Premovic <lazar2premovic@gmail.com>
2016-04-23 22:36:55 +0200

    Beginig of the map

---
+++ OpenDataProject/OpenDataProject_Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenDataProject
+{
+    public partial class OpenDataProject_Map : Form
+    {
+        public OpenDataProject_Map()
+        {
+            InitializeComponent();
+        }
+    }
+}
